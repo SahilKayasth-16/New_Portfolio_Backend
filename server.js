5ef130bb12e69495564aa4cef2a4607e199d2c45
SahilKayasth-16 <sahilkayasth1612@gmail.com>
2026-05-10 20:03:05 +0530

    fix server.js to make contact form work properly

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://sahilkportfolio.vercel.app/"
+      "https://sahilkportfolio.vercel.app"
     ]
   }));
 app.use(express.json());
